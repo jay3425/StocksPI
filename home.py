075e1f7b282aebc209571f7da54a762271a71bd9
Jay_Dengle <jaydengle2005@gmail.com>
2026-05-08 09:21:33 +0530

    Update home.py

--- home.py
+++ home.py
@@ -133,7 +133,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css(r"C:\Users\user\OneDrive\Desktop\Sector Analyzer\style.css")
+local_css(r"style.css")
 
 animation_symbole = "💶"
 
